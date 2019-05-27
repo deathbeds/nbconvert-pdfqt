5ed0b214f03bad80146c2b4579d78b974a76dd49
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-05-27 18:38:17 -0400

    more stuff for xvfb

--- src/nbconvert_pdfqt/printer.py
+++ src/nbconvert_pdfqt/printer.py
@@ -9,77 +9,7 @@ from six.moves.urllib.request import urlopen
 
 from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets, QtGui
 
-
-LAB_STYLE = """
-body {
-    --jp-layout-color0: transparent;
-    --jp-layout-color1: transparent;
-    --jp-layout-color2: transparent;
-    --jp-layout-color3: transparent;
-    --jp-cell-editor-background: transparent;
-    --jp-border-width: 0;
-    --jp-border-color0: transparent;
-    --jp-border-color1: transparent;
-    --jp-border-color2: transparent;
-}
-body,
-body .jp-ApplicationShell {
-    width: 1920px;
-    height: 2024px;
-}
-body .jp-InputArea-editor {
-    border: 0;
-    padding-left: 2em;
-}
-body .jp-LabShell.jp-mod-devMode {
-    border: 0 !important;
-}
-body .jp-InputPrompt,
-body .jp-OutputPrompt {
-    display: none;
-    min-width: 0;
-    max-width: 0;
-}
-body #jp-main-dock-panel {
-    padding: 0;
-}
-body .jp-SideBar.p-TabBar,
-body #jp-top-panel,
-body .jp-Toolbar,
-body .p-DockPanel-tabBar,
-body .jp-Collapser {
-    display: none;
-    min-height: 0;
-    max-height: 0;
-    min-width: 0;
-    max-width: 0;
-}
-"""
-
-FORCE_DEV = """
-var cfg = document.querySelector("#jupyter-config-data");
-if(cfg) {
-    cfg.textContent = cfg.textContent.replace(
-        `"devMode": "False"`,
-        `"devMode": "True"`
-    )
-}
-"""
-
-SINGLE_DOCUMENT = """
-window.lab.shell.removeClass('jp-mod-devMode');
-let cmd = window.lab.commands;
-setTimeout(function(){
-    cmd.execute('application:set-mode', {mode: 'single-document'});
-    if(document.querySelector('body[data-left-sidebar-widget]')) {
-        cmd.execute('application:toggle-left-area');
-    }
-}, 2000);
-setTimeout(function(){
-    console.error('running all cells');
-    cmd.execute('notebook:run-all-cells');
-}, 3000);
-"""
+from .static import SINGLE_DOCUMENT, RUN_ALL, LAB_STYLE, FORCE_DEV
 
 
 class QPDFPage(QtWebEngineWidgets.QWebEnginePage):
@@ -103,7 +33,8 @@ class QPDFPage(QtWebEngineWidgets.QWebEnginePage):
             var style = document.createElement("style");
             style.textContent = `{LAB_STYLE}`;
             document.body.appendChild(style);
-            {SINGLE_DOCUMENT}
+            {SINGLE_DOCUMENT % 2000}
+            {RUN_ALL}
         """)
         QtCore.QTimer.singleShot(8000, lambda *_: self.measure())
 
@@ -111,6 +42,10 @@ class QPDFPage(QtWebEngineWidgets.QWebEnginePage):
         print("measuring")
         self.runJavaScript(f"""
             var bb = document.querySelector(".jp-Cell:last-child").getBoundingClientRect();
+            var node = document.querySelector(".jp-ApplicationShell").style;
+            var body = document.body.style;
+            node.height = node.minHeight = body.height = body.minHeight = bb.bottom + "px";
+            {SINGLE_DOCUMENT % 0}
             [bb.bottom, bb.right]
         """, lambda size: self.emit_pdf(size))
 
