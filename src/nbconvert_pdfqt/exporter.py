aebbd3655d278baf295c0972fb349ecc3cfb7cf1
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-05-26 23:30:13 -0400

    split up printer and exporter

--- src/nbconvert_pdfqt/exporter.py
+++ src/nbconvert_pdfqt/exporter.py
@@ -8,75 +8,6 @@ import nbformat
 
 from nbconvert.exporters.html import HTMLExporter
 
-LAB_STYLE = """
-body {
-    --jp-layout-color0: transparent;
-    --jp-layout-color1: transparent;
-    --jp-layout-color2: transparent;
-    --jp-layout-color3: transparent;
-    --jp-cell-editor-background: transparent;
-}
-body .jp-InputArea-editor {
-    border: 0;
-    padding-left: 2em;
-}
-body .jp-LabShell.jp-mod-devMode {
-    border: 0 !important;
-}
-body .jp-InputPrompt,
-body .jp-OutputPrompt {
-    display: none;
-    min-width: 0;
-    max-width: 0;
-}
-body #jp-main-dock-panel {
-    padding: 0;
-}
-body .jp-SideBar.p-TabBar {
-    min-width: 0;
-    max-width: 0;
-    display: none;
-}
-body #jp-top-panel {
-    display: none;
-    min-height: 0;
-    max-height: 0;
-}
-body .jp-Toolbar {
-    display: none;
-    min-height: 0;
-    max-height: 0;
-}
-body .p-DockPanel-tabBar {
-    display: none;
-    min-height: 0;
-    max-height: 0;
-}
-"""
-
-FORCE_DEV = """
-var cfg = document.querySelector("#jupyter-config-data");
-if(cfg) {
-    cfg.textContent = cfg.textContent.replace(
-        `"devMode": "False"`,
-        `"devMode": "True"`
-    )
-}
-"""
-
-SINGLE_DOCUMENT = """
-let cmd = window.lab.commands;
-setTimeout(function(){
-    cmd.execute('application:set-mode', {mode: 'single-document'});
-    if(document.querySelector('body[data-left-sidebar-widget]')) {
-        cmd.execute('application:toggle-left-area');
-    }
-}, 2000);
-setTimeout(function(){
-    console.error('running all cells');
-    cmd.execute('notebook:run-all-cells');
-}, 3000);
-"""
 
 class PDFQtExporter(HTMLExporter):
     def from_notebook_node(self, nb, resources=None, **kw):
@@ -105,9 +36,14 @@ class PDFQtExporter(HTMLExporter):
 
             subprocess.check_call([
                 sys.executable,
-                "-m", "nbconvert_pdfqt.exporter",
+                "-m", "nbconvert_pdfqt.printer",
                 td
-            ])
+            ], env=dict(
+                **os.environ,
+                QTWEBENGINE_DISABLE_SANDBOX="1",
+                QT_WEBENGINE_DISABLE_GPU="1",
+                QTWEBENGINE_CHROMIUM_FLAGS="--headless --disable-gpu --no-sandbox --single-process --enable-logging --log-level=0"
+            ))
 
             pdf_file = "notebook.pdf"
 
@@ -127,101 +63,3 @@ class PDFQtExporter(HTMLExporter):
         resources.pop('outputs', None)
 
         return pdf_data, resources
-
-
-def print_pdf():
-    from uuid import uuid4
-    import socket
-    import time
-
-    from six.moves.urllib.request import urlopen
-
-    from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets, QtGui
-
-    host = "localhost"
-    token = str(uuid4())
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, 0))
-    s.listen(1)
-    port = s.getsockname()[1]
-    s.close()
-
-
-    lab = subprocess.Popen([
-        sys.executable,
-        "-m",
-        "jupyterlab.labapp",
-        "--port={}".format(port),
-        "--NotebookApp.token='{}'".format(token),
-        "--no-browser"
-    ])
-    url = "http://{}:{}/lab/tree/notebook.ipynb?token={}".format(host, port, token)
-    shutdown_url = "http://{}:{}/api/shutdown?token={}".format(host, port, token)
-
-    retries = 10
-
-    while retries:
-        time.sleep(1)
-        retries -= 1
-        try:
-            urlopen(url)
-            break
-        except:
-            pass
-
-    app = QtWidgets.QApplication(["-platform", "offscreen"])
-    web = QtWebEngineWidgets.QWebEngineView()
-
-    def started(*args):
-        web.page().runJavaScript(FORCE_DEV)
-
-    def wait_for_content(finished):
-        web.show()
-        web.page().runJavaScript(f"""
-            var style = document.createElement("style");
-            style.textContent = `{LAB_STYLE}`;
-            document.body.appendChild(style);
-            {SINGLE_DOCUMENT}
-        """)
-        web.setFixedSize(1000, 10000)
-        QtCore.QTimer.singleShot(8000, measure)
-
-    def measure():
-        web.page().runJavaScript(f"""
-            var bb = document.querySelector(".jp-Cell:last-child").getBoundingClientRect();
-            [bb.bottom, bb.right]
-        """, emit_pdf)
-
-    def emit_pdf(size):
-        screen = app.screens()[0]
-        dpi = screen.physicalDotsPerInch()
-        print("SIZE", size, dpi)
-        x = 2.5
-        dims = size[0] / dpi * x, size[1] / dpi * x
-        page_size = QtGui.QPageSize(QtCore.QSizeF(*dims), QtGui.QPageSize.Inch)
-        layout = QtGui.QPageLayout()
-        layout.setPageSize(page_size)
-
-        web.page().printToPdf(
-            os.path.join(os.getcwd(), "notebook.pdf"),
-            layout
-        )
-
-    def emitted_pdf(*args):
-        app.quit()
-
-    web.loadProgress.connect(started)
-    web.loadFinished.connect(wait_for_content)
-    web.page().pdfPrintingFinished.connect(emitted_pdf)
-
-    web.load(QtCore.QUrl(url))
-
-    try:
-        app.exec_()
-    finally:
-        urlopen(shutdown_url, data=[])
-        lab.wait()
-
-if __name__ == "__main__":
-    print_pdf()
---
+++ src/nbconvert_pdfqt/printer.py
@@ -0,0 +1,191 @@
+import sys
+import os
+from uuid import uuid4
+import socket
+import time
+import subprocess
+
+from six.moves.urllib.request import urlopen
+
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets, QtGui
+
+
+LAB_STYLE = """
+body {
+    --jp-layout-color0: transparent;
+    --jp-layout-color1: transparent;
+    --jp-layout-color2: transparent;
+    --jp-layout-color3: transparent;
+    --jp-cell-editor-background: transparent;
+}
+body,
+body .jp-ApplicationShell {
+    width: 1920px;
+    height: 2024px;
+}
+body .jp-InputArea-editor {
+    border: 0;
+    padding-left: 2em;
+}
+body .jp-LabShell.jp-mod-devMode {
+    border: 0 !important;
+}
+body .jp-InputPrompt,
+body .jp-OutputPrompt {
+    display: none;
+    min-width: 0;
+    max-width: 0;
+}
+body #jp-main-dock-panel {
+    padding: 0;
+}
+body .jp-SideBar.p-TabBar,
+body #jp-top-panel,
+body .jp-Toolbar,
+body .p-DockPanel-tabBar,
+body .jp-Collapser {
+    display: none;
+    min-height: 0;
+    max-height: 0;
+    min-width: 0;
+    max-width: 0;
+}
+"""
+
+FORCE_DEV = """
+var cfg = document.querySelector("#jupyter-config-data");
+if(cfg) {
+    cfg.textContent = cfg.textContent.replace(
+        `"devMode": "False"`,
+        `"devMode": "True"`
+    )
+}
+"""
+
+SINGLE_DOCUMENT = """
+let cmd = window.lab.commands;
+setTimeout(function(){
+    cmd.execute('application:set-mode', {mode: 'single-document'});
+    if(document.querySelector('body[data-left-sidebar-widget]')) {
+        cmd.execute('application:toggle-left-area');
+    }
+}, 2000);
+setTimeout(function(){
+    console.error('running all cells');
+    cmd.execute('notebook:run-all-cells');
+}, 3000);
+"""
+
+
+class QPDFPage(QtWebEngineWidgets.QWebEnginePage):
+    dpi = None
+    scale_hack = 2.5
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.loadFinished.connect(self._load_finished)
+        self.loadProgress.connect(self._load_progress)
+        self.setZoomFactor(1)
+
+    def _load_progress(self, *args):
+        print("progress")
+        self.runJavaScript(FORCE_DEV)
+
+    def _load_finished(self, finished):
+        print("finished")
+        self.runJavaScript(f"""
+            var style = document.createElement("style");
+            style.textContent = `{LAB_STYLE}`;
+            document.body.appendChild(style);
+            {SINGLE_DOCUMENT}
+        """)
+        QtCore.QTimer.singleShot(8000, lambda *_: self.measure())
+
+    def measure(self):
+        print("measuring")
+        self.runJavaScript(f"""
+            var bb = document.querySelector(".jp-Cell:last-child").getBoundingClientRect();
+            [bb.bottom, bb.right]
+        """, lambda size: self.emit_pdf(size))
+
+    def emit_pdf(self, size):
+        print("QSIZE", self.contentsSize().__dict__)
+        print("emitting", size)
+        print("SIZE", size, self.dpi)
+        # dims = size[0] / self.dpi * self.scale_hack, size[1] / self.dpi * self.scale_hack
+        dims = size[0] / 72, size[1] / 72
+        page_size = QtGui.QPageSize(QtCore.QSizeF(10, 10), QtGui.QPageSize.Inch)
+        layout = QtGui.QPageLayout()
+        layout.setPageSize(page_size)
+        self.printToPdf(
+            os.path.join(os.getcwd(), "notebook.pdf"),
+            layout
+        )
+        print("emitted")
+
+
+def print_pdf():
+    host = "localhost"
+    token = str(uuid4())
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, 0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+
+    lab = subprocess.Popen([
+        sys.executable,
+        "-m",
+        "jupyterlab.labapp",
+        "--port={}".format(port),
+        "--NotebookApp.token='{}'".format(token),
+        "--no-browser"
+    ])
+    url = "http://{}:{}/lab/tree/notebook.ipynb?token={}".format(host, port, token)
+    shutdown_url = "http://{}:{}/api/shutdown?token={}".format(host, port, token)
+
+    retries = 10
+
+    while retries:
+        time.sleep(1)
+        retries -= 1
+        try:
+            urlopen(url)
+            break
+        except:
+            pass
+
+    app = QtWidgets.QApplication([])
+    screen = app.screens()[0]
+
+    page = QPDFPage()
+    page.dpi = screen.physicalDotsPerInch()
+
+    def shutdown():
+        try:
+            urlopen(shutdown_url, data=[])
+        except:
+            pass
+        finally:
+            lab.wait()
+        try:
+            app.quit()
+        except:
+            pass
+
+
+    # view.page().pdfPrintingFinished.connect(lambda *_: shutdown())
+    # view.load(QtCore.QUrl(url))
+    page.pdfPrintingFinished.connect(lambda *_: shutdown())
+    page.load(QtCore.QUrl(url))
+
+    try:
+        app.exec_()
+    finally:
+        shutdown()
+
+
+if __name__ == "__main__":
+    print_pdf()
